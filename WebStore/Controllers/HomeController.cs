cabacc93ed8721ed57833dbafb39e3d4cbf4fc7d
solomon9182 <solomon9182@gmail.com>
2020-03-09 18:38:27 +0300

    Сотрудники выделены в отдельный контроллер

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -7,45 +7,13 @@ namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        private static readonly List<Employee> _Employee = new List<Employee> {
-            new Employee{
-                id = 1,
-                Patronymic = "Иванов",
-                SurName = "Иван",
-                FirstName = "Иванович",
-                Age = 39
-            },
-            new Employee{
-                id = 2,
-                Patronymic = "Петров",
-                SurName = "Пётр",
-                FirstName = "Петрович",
-                Age = 18
-            },
-            new Employee{
-                id = 3,
-                Patronymic = "Сидоров",
-                SurName = "Сидор",
-                FirstName = "Сидорович",
-                Age = 27
-            },
-        };
+        
 
 
         public IActionResult Index() => View();
 
 
         public IActionResult SomeAction() => Content("2");
-
-        public IActionResult Employees() => View(_Employee);
-
-        public IActionResult Employee(int id) 
-        {
-            var employee = _Employee.FirstOrDefault(e => e.id == id);
-            if (employee is null)
-                return NotFound();
-            return View(employee);
-        }
 
         public IActionResult Error404() => View();
         public IActionResult Blog() => View();
