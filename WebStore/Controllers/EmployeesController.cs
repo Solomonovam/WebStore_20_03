95f0bc5b3636ec2369c60cf9f6740549a87ea2db
solomon9182 <solomon9182@gmail.com>
2020-03-12 22:05:41 +0300

    Сервис управления сотрудниками

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Data;
 using WebStore.Models;
 
 namespace WebStore.Controllers
@@ -10,36 +11,13 @@ namespace WebStore.Controllers
     //[Route("users")]
     public class EmployeesController : Controller
     {
-        private static readonly List<Employee> _Employee = new List<Employee> {
-            new Employee{
-                id = 1,
-                Patronymic = "Иванов",
-                SurName = "Иван",
-                FirstName = "Иванович",
-                Age = 39
-            },
-            new Employee{
-                id = 2,
-                Patronymic = "Петров",
-                SurName = "Пётр",
-                FirstName = "Петрович",
-                Age = 18
-            },
-            new Employee{
-                id = 3,
-                Patronymic = "Сидоров",
-                SurName = "Сидор",
-                FirstName = "Сидорович",
-                Age = 27
-            },
-        };
 
         //[Route("employees")]
-        public IActionResult Index() => View(_Employee);
+        public IActionResult Index() => View(TestData.Employees);
 
         //[Route("employee/{id}")]
         public IActionResult Details(int id) {
-            var employee = _Employee.FirstOrDefault(e => e.id == id);
+            var employee = TestData.Employees.FirstOrDefault(e => e.id == id);
             if (employee is null)
                 return NotFound();
             return View(employee);
