ca0903069a4158cc0a6db2c2eb430b0e757a9d51
solomon9182 <solomon9182@gmail.com>
2020-03-18 21:17:08 +0300

    Подготовка сущностей

--- WebStore.Domain/Entities/Base/BaseEntity.cs
+++ WebStore.Domain/Entities/Base/BaseEntity.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Text;
 using WebStore.Domain.Entities.Base.Interfaces;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebStore.Domain.Entities.Base
 {
     public abstract class BaseEntity : IBaseEntity
     {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set;}
     }
 }
--- WebStore.Domain/Entities/Base/NamedEntity.cs
+++ WebStore.Domain/Entities/Base/NamedEntity.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Text;
 using WebStore.Domain.Entities.Base.Interfaces;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebStore.Domain.Entities.Base
 {
     public abstract class NamedEntity : BaseEntity, INamedEntity
     {
+        [Required/*, StringLength(250)*/]
         public string Name { get; set; }
     }
 }
