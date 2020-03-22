fb1052c5639f66b66309a15c0a45c5c7fe8e0b37
solomon9182 <solomon9182@gmail.com>
2020-03-22 17:04:03 +0300

    Модели представления Регистрации и входа

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.ViewModels.Identity;
 
 namespace WebStore.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Register() => View();
-        public IActionResult Login() => View();
+        public IActionResult Register() => View(new RegisterUserViewModel());
+        public IActionResult Login() => View(new LoginViewModel());
 
     }
 }
